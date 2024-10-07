39948863ad65ada4f3f047782c1a5cf90da067bd
Joey van Noort <joey@lvannoort.com>
2024-10-07 20:52:01 +0200

    Cleaned up code

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import socket
 UDP_IP = "0.0.0.0"
 UDP_PORT = 5000    # Replace with the actual port used by CanEthernet
 
+SENDER_IP = "192.168.2.28"
+
 # Define the CanEthernetPacketXL header structure
 HEADER_FORMAT = '<HHBH'  # Adjust according to the actual structure
 HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
-print(f"Header size: {HEADER_SIZE}")
-# Define the structure of your packet.
+
+
+
+
 class DriveDataPacket:
     def __init__(self, node_id, amps, rpm, fettemp):
         self.node_id = node_id
@@ -19,21 +23,15 @@ class DriveDataPacket:
     def __str__(self):
         return f"Node ID: {self.node_id}, Amps: {self.amps}, RPM: {self.rpm}, FET Temp: {self.fettemp}"
 
-# Define a function to unpack the packet.
+
 def unpack_drive_data_packet(data):
     # The format string here corresponds to:
     # 1 byte for nodeId, 12 floats for amps, rpm, and fettemp (4 for each array).
-    # node_id_format = "!B"
-    floats_format_str = "<B12f"  # B = unsigned char (1 byte), f = float (4 bytes)
+
+    payload_format_string = "<B12f"  # B = unsigned char (1 byte), f = float (4 bytes)
    
-    # Unpack the Node ID
-    # node_id = struct.unpack(node_id_format, data[:2])[0]
-
-    # print(f"Received data in hex: {' '.join(f'{byte:02x}' for byte in data[2:])}")
-    # print(len(data[2:]))
-
     # Unpack the binary data.
-    floats = struct.unpack(floats_format_str, data)
+    floats = struct.unpack(payload_format_string, data)
     print(f"Received data in hex: {' '.join(f'{byte:02x}' for byte in data)}")
     
     # Extract the unpacked values.
@@ -109,7 +107,7 @@ def main():
     while True:
         data, addr = sock.recvfrom(512)  # Buffer size is 2048 bytes
         print(f"Received data in hex: {' '.join(f'{byte:02x}' for byte in data)}")
-        if addr[0] != '192.168.2.28':
+        if addr[0] != SENDER_IP:
             continue
         packet = parse_packet(data)
         print(packet)
